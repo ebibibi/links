a2f87c92f455c6e50a92ae2b5a1d635905471c77
Masahiko Ebisuda <ebibibi@gmail.com>
2012-11-04 21:07:31 +0900

    delete duplicate <script> tag for hatebu

--- server.rb
+++ server.rb
@@ -66,7 +66,7 @@ helpers do
   end
 
   def hatebu(url)
-    '<a href="http://b.hatena.ne.jp/entry/' + url + '" class="hatena-bookmark-button" data-hatena-bookmark-layout="simple-balloon" title="このエントリーをはてなブックマークに追加"><img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>'
+    '<a href="http://b.hatena.ne.jp/entry/' + url + '" class="hatena-bookmark-button" data-hatena-bookmark-layout="simple-balloon" title="このエントリーをはてなブックマークに追加"><img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a>'
   end
 
   def twitter(url)
