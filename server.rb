e2214fb472a4006602f5bf9be8a31022b36d454f
Masahiko Ebisuda <ebibibi@gmail.com>
2012-11-04 21:16:41 +0900

    delete duplicate <script> tag for twitter

--- server.rb
+++ server.rb
@@ -70,7 +70,7 @@ helpers do
   end
 
   def twitter(url)
-    '<a href="https://twitter.com/share" class="twitter-share-button" data-url="' + url + '" data-via="" data-lang="ja">ツイート</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>'
+    '<a href="https://twitter.com/share" class="twitter-share-button" data-url="' + url + '" data-via="" data-lang="ja">ツイート</a>'
   end
 
 end
